63ff4df8dd3240135ea50b7648ecb12cd3fac17f
Lorenzo Lomartire <97639913+LL-10@users.noreply.github.com>
2022-11-14 22:25:52 +0100

    #

--- partizandegrado/modules/HTMLpage.js
+++ partizandegrado/modules/HTMLpage.js
@@ -1,19 +1,22 @@
 import {HTMLelement, html, head, title, body} from './HTMLelement.js';
 
 class HTMLpage {
-    #title;
-    #build;
-    constructor(title, ...contents) {
-        class Element extends HTMLelement {
-            
-        }
-        this.#title = title;
-        this.#build = contents;
-    }
-    build() {
-        title.init(this.#title)
-        this.#build();
-    }
+ #title;
+ #build;
+ constructor(title, reset, ...contents) {
+  class Element extends HTMLelement {
+   
+  }
+  this.#title = title;
+  this.#build = contents;
+ }
+ build() {
+  if (reset) {
+   body.clear();
+  }
+  this.#build();
+  title.init(this.#title);
+ }
 }
 
 export default HTMLpage;
---
+++ partizandegrado/script.js
@@ -0,0 +1,22 @@
+import './modules/default.js';
+import {html, head, title, body} from './modules/HTMLelement.js';
+import Page from './modules/HTMLpage.js';
+
+const HOME = new Page("Partizan Degrado - Pagina Home", true,()=>{
+ let link1 = head.append("link");
+ let link2 = head.append("link");
+ link1.rel = "stylesheet"
+ link1.href = "https://fonts.googleapis.com/css?family=Open+Sans&display=swap"
+ link2.rel = "stylesheet"
+ link2.href = "style/menu.css"
+ let menu = body.append('div');
+ let list = menu.append('ul');
+ let l1 = list.append('li');
+ let l2 = list.append('li');
+ l1.href = "index.html"
+ l1.innerHTML = "Pagina Home"
+ l2.href = "login.html"
+ l2.innerHTML = "Area Membri"
+});
+
+HOME.build();
