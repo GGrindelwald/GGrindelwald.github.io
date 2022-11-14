278dd636207bdd7528fc7db313d3e085503b1f46
GGrindelwald <98670687+GGrindelwald@users.noreply.github.com>
2022-11-14 22:37:49 +0100

    update HTMLpage.js

--- partizandegrado/modules/HTMLpage.js
+++ partizandegrado/modules/HTMLpage.js
@@ -3,11 +3,13 @@ import {HTMLelement, html, head, title, body} from './HTMLelement.js';
 class HTMLpage {
  #title;
  #build;
+ #reset;
  constructor(title, reset, ...contents) {
   class Element extends HTMLelement {
    
   }
   this.#title = title;
+  this.#reset = !!reset;
   this.#build = contents;
  }
  build() {
